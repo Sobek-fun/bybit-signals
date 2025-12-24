b698fc60d337a34602db64a2ed6246bea05dad42
Bohdan Hulko <bohdan.hulko@icloud.com>
2025-12-24 13:00:31 +0100

    perf: remove unused technical indicators to reduce CPU overhead

--- src/monitoring/indicator_calculator.py
+++ src/monitoring/indicator_calculator.py
@@ -6,9 +6,6 @@ class IndicatorCalculator:
     def calculate(self, df: pd.DataFrame) -> pd.DataFrame:
         df.ta.mfi(length=14, append=True)
         df.ta.rsi(length=14, append=True)
-        df.ta.stochrsi(length=14, rsi_length=14, k=3, d=3, append=True)
         df.ta.macd(fast=12, slow=26, signal=9, append=True)
-        df.ta.fisher(length=9, append=True)
-        df.ta.ad(append=True)
 
         return df
