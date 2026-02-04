c204de2ea97bd3f6bd28d6ad5f14c86c299c467a
qyc71kk <bohdan.hulko@icloud.com>
2026-02-04 12:12:38 +0100

    revert: labeler for pump long model

--- pump_long/labeler.py
+++ pump_long/labeler.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from typing import Optional
 
-from src.shared.pump.labeler_lookahead import PumpLabelerLookahead
+from pump_long.labeler_lookahead import PumpLabelerLookahead
 
 
 class PumpStartLabelerLookahead:
